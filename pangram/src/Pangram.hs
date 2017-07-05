acb8ad6f19b1d3e8114908d8b9ac717da2d77553
GoldenPalladin <p.yakovlev@live.ru>
2017-07-05 21:47:27 +0300

    Exercism.io First three Haskell tutorials

--- pangram/src/Pangram.hs
+++ pangram/src/Pangram.hs
@@ -1,4 +1,12 @@
 module Pangram (isPangram) where
+import Data.Char
+import Data.List
 
 isPangram :: String -> Bool
-isPangram text = error "You need to implement this function."
+isPangram text = uniqueLiteralsCount (purifyString text) == 26
+
+purifyString :: String -> String
+purifyString xs  = sort $ map toLower $ filter isAlpha xs
+
+uniqueLiteralsCount :: String -> Int
+uniqueLiteralsCount xs = foldl (\acc x -> acc+1) 0 $ group xs
