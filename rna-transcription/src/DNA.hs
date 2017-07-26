141274e2afb48866341c9ee4305b287d6407d3ec
GoldenPalladin <p.yakovlev@live.ru>
2017-07-26 15:59:48 +0300

    DNA transcription task done

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -1,4 +1,14 @@
 module DNA (toRNA) where
 
 toRNA :: String -> Maybe String
-toRNA xs = error "You need to implement this function."
+toRNA x =
+  if filter (`elem` ['C','G','T','A']) x == x
+  then Just (map toRNAamin x)
+  else Nothing
+  where
+    toRNAamin amin = case amin of
+      'C' -> 'G'
+      'G' -> 'C'
+      'T' -> 'A'
+      'A' -> 'U'
+      _ -> ' '
