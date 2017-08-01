b4fbfcbc6a11c4249c09e7102048c96db5fe1af2
GoldenPalladin <p.yakovlev@live.ru>
2017-08-01 17:14:22 +0300

    Acronym solved

--- acronym/src/Acronym.hs
+++ acronym/src/Acronym.hs
@@ -1,4 +1,11 @@
 module Acronym (abbreviate) where
+import Data.Char (isUpper, isAlpha, isSpace, toUpper)
 
 abbreviate :: String -> String
-abbreviate xs = error "You need to implement this function."
+abbreviate = concatMap processWord.words.filter isAlphaSpace.replaceHyphen
+  where isAlphaSpace x = isAlpha x || isSpace x
+        replaceHyphen = map (\x-> if x=='-' then ' ' else x)
+        processWord x
+          | (upF x == x)||(upF x == "") = [toUpper (head x)]
+          | otherwise = upF x
+        upF = filter isUpper
