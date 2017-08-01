add9f5afe4ab237d6aebeb2dc3cd991d4337004e
GoldenPalladin <p.yakovlev@live.ru>
2017-08-01 15:46:08 +0300

    Grains problem solved

---
+++ grains/src/Grains.hs
@@ -0,0 +1,9 @@
+module Grains (square, total) where
+
+square :: Integer -> Maybe Integer
+square n
+  | (n > 0) && (n < 65) = Just $ 2 ^ (n-1)
+  | otherwise = Nothing
+
+total :: Integer
+total = sum $ map (2 ^) [0..63]
